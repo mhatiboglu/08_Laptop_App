51a4e4862b09bee84c6cbd869a03adc4974e8aaa
mhatiboglu <mh1741mh@gmail.com>
2020-02-22 22:41:53 +0200

    Create laptop page templates.

--- index.js
+++ index.js
@@ -14,8 +14,23 @@ const server = http.createServer((req, res) => {
     res.end("This is the PRODUCTS page");
   } else if (pathName === "/laptop" && id < laptopData.length) {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end(`This is the LAPTOP page for ${id}`);
-  } else {
+    fs.readFile(
+      `${__dirname}/templates/template-laptop.html`,
+      "utf-8",
+      (err, data) => {
+        const laptop = laptopData[id];
+        let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+        output = output.replace(/{%IMAGE%}/g, laptop.image);
+        output = output.replace(/{%PRICE%}/g, laptop.price);
+        output = output.replace(/{%SCREEN%}/g, laptop.screen);
+        output = output.replace(/{%CPU%}/g, laptop.cpu);
+        output = output.replace(/{%STORAGE%}/g, laptop.storage);
+        output = output.replace(/{%RAM%}/g, laptop.ram);
+        output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+        res.end(output);
+      }
+    );
+    } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("URL was not found on the server!");
   }
